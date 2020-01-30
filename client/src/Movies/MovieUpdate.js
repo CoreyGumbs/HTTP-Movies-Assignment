27bbee020adf3a329889547b67d3780ff3467e20
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-30 16:18:15 -0500

    [Initial] Initial File setup

---
+++ client/src/Movies/MovieUpdate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const MovieUpdate = () => {
+    return(
+        <div className="movie-update-container">
+
+        </div>
+    )
+}
+
+export default MovieUpdate;
