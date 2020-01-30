1a2acfd34551fa7e66a88b89e659ef04a81038aa
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-30 17:02:09 -0500

    [Add] changes movie update to class component, added change handler

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -1,30 +1,56 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 
-const MovieUpdate = () => {
-    return(
-        <div className="movie-update-container">
-            <form className="movie-update-form">
-                <div className="movie-update-input-container">
-                    <label htmlFor="" className="movie-update-label"></label>
-                    <input type="text" className="movie-update-input"/>
-                </div>
-                <div className="movie-update-input-container">
-                    <label htmlFor="" className="movie-update-label"></label>
-                    <input type="text" className="movie-update-input"/>
-                </div>
-                <div className="movie-update-input-container">
-                    <label htmlFor="" className="movie-update-label">
+class MovieUpdate extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            title: '',
+            director: '',
+            metascore: 0,
+            stars: []
+        }
+    }
+
+    handleChanges =  e => {
+        console.log(e.target.name)
+    }
+
+    render(){
+        
+        const {title, director, metascore, stars} =  this.state;
+
+        return(
+            <div className="movie-card">
+                <form className="movie-update-form">
+                    <div className="movie-update-input-container">
+                        <label htmlFor="" className="movie-update-label">
+                            Title:
                         </label>
-                        <input type="text" className="movie-update-input"/>
+                        <input type="text" className="movie-update-input" name="title " value={title}/>
                     </div>
-                <div className="movie-update-input-container">
-                    <label htmlFor="" className="movie-update-label"></label>
-                    <input type="text" className="movie-update-input"/>
-                </div>
-            </form>
-        </div>
-    )
+                    <div className="movie-update-input-container">
+                        <label htmlFor="" className="movie-update-label" >
+                            Director:
+                        </label>
+                        <input type="text" className="movie-update-input"  name="director" value={director}/>
+                    </div>
+                    <div className="movie-update-input-container">
+                        <label htmlFor="" className="movie-update-label">
+                            Metascore: 
+                        </label>
+                            <input type="number" className="movie-update-input" name="metascore" value={metascore}/>
+                        </div>
+                    <div className="movie-update-input-container">
+                        <label htmlFor="" className="movie-update-label">
+                            Actors: 
+                        </label>
+                        <input type="text" className="movie-update-input" name="stars" value={stars} />
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
 
 export default MovieUpdate;
