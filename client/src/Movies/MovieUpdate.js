a84c7836bddcda8e4fc129c9c52b491ccbe6b8f1
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-30 16:26:19 -0500

    [Add] Added Movie Update Form

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -4,7 +4,25 @@ import React from 'react';
 const MovieUpdate = () => {
     return(
         <div className="movie-update-container">
-
+            <form className="movie-update-form">
+                <div className="movie-update-input-container">
+                    <label htmlFor="" className="movie-update-label"></label>
+                    <input type="text" className="movie-update-input"/>
+                </div>
+                <div className="movie-update-input-container">
+                    <label htmlFor="" className="movie-update-label"></label>
+                    <input type="text" className="movie-update-input"/>
+                </div>
+                <div className="movie-update-input-container">
+                    <label htmlFor="" className="movie-update-label">
+                        </label>
+                        <input type="text" className="movie-update-input"/>
+                    </div>
+                <div className="movie-update-input-container">
+                    <label htmlFor="" className="movie-update-label"></label>
+                    <input type="text" className="movie-update-input"/>
+                </div>
+            </form>
         </div>
     )
 }
