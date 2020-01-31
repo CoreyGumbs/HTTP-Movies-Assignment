c794df463043fbecb449288526928b2f0f067fe9
Corey Gumbs <5513859+CoreyGumbs@users.noreply.github.com>
2020-01-30 22:53:40 -0500

    [Add] Added Put, test and working.

--- client/src/Movies/MovieUpdate.js
+++ client/src/Movies/MovieUpdate.js
@@ -5,6 +5,7 @@ class MovieUpdate extends Component {
     constructor(props){
         super(props);
         this.state = {
+            id: '',
             title: '',
             director: '',
             metascore: '',
@@ -20,6 +21,7 @@ class MovieUpdate extends Component {
         axios.get(`http://localhost:5000/api/movies/${this.props.match.params.id}`)
         .then(res => {
             this.setState({
+                id: res.data.id,
                 title: res.data.title,
                 director: res.data.director,
                 metascore: res.data.metascore,
@@ -41,43 +43,46 @@ class MovieUpdate extends Component {
 
     handleSubmit = e => {
         e.preventDefault();
-        const {title, director, metascore, stars} = this.state;
+        const {id, title, director, metascore, stars} = this.state;
         
         let starsArray;
+
         if(!Array.isArray(stars)){
-            starsArray = stars.split(',')
+            starsArray = stars.split(',');
+        }else{
+            starsArray = stars
         };
         
         const body = {
+            id: id,
             title: title,
             director: director,
             metascore: metascore,
-            stars: starsArray
+            stars: starsArray 
         }
 
         this.updateMovie(body);
     }
 
     updateMovie = movie => {
+        console.log(movie, `http://localhost:5000/api/movies/${this.props.match.params.id}`);
 
         axios.put(`http://localhost:5000/api/movies/${this.props.match.params.id}`, movie)
-        .then(res =>{
-            console.log(res);
-        })
+        .then(res => console.log(res))
         .catch(err => console.log(err));
 
-        this.props.history.push(`/movies/${this.props.match.params.id}`);
+        this.props.history.push(`/`);
     }
 
     render(){
-        console.log(this.state.title);
         const {title, director, metascore, stars} =  this.state;
 
         return(
-            <div className="movie-card">
+            <div className="update-container">
+                <h1>Update Movie</h1>
                 <form className="movie-update-form" onSubmit={this.handleSubmit}>
                     <div className="movie-update-input-container">
-                        <label htmlFor="" className="movie-update-label">
+                        <label htmlFor="title" className="movie-update-label">
                             Title:
                         </label>
                         <input type="text" className="movie-update-input" name="title" value={ title} onChange={this.handleChanges} required/>
